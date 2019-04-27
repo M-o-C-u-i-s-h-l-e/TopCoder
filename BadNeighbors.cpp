7b71365c9438f5055231d4067a4c68c527d074b2
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-27 13:50:09 +0530

    Create BadNeighbors.cpp

---
+++ BadNeighbors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int start, int end, vector<int> &donations) {
+	int a = 0, b = 0, c = 0;
+    for (int i = start; i < end; i++) {
+    	int Max = max(a, b);
+        a = Max, b = c, c = Max + donations[i];
+    }
+    return max(a, max(b, c));
+}
+
+class BadNeighbors {
+	public:
+    int maxDonations(vector <int> donations) {
+    	int t1 = func(0, donations.size()-1, donations);
+        int t2 = func(1, donations.size(), donations);
+        return max(t1, t2);
+    }
+};
