e0a7469f730e419c53ac9d1a9738fd170c6d31a8
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-27 17:47:35 +0530

    Update FlowerGarden.cpp

--- FlowerGarden.cpp
+++ FlowerGarden.cpp
@@ -1,3 +1,4 @@
+// O(n ^ 3)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -26,5 +27,46 @@ class FlowerGarden {
             v[i] = height[pos], visited[pos] = true;
         }
         return v;
+    }
+};
+
+
+
+// O(n ^ 2)
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, arr[50][50];
+vector<int> ans;
+vector<pair<int, pair<int, int>>> v;
+vector<bool> visited;
+
+void sol(int i) {
+	if (!visited[i]) {
+		for (int j = 0; j < n; j++)
+			if (arr[i][j] != i)
+				sol(arr[i][j]);
+		ans.push_back(v[i].first);
+		visited[i] = true;
+	}
+}
+
+class FlowerGarden {
+	public:
+    vector <int> getOrdering(vector <int> height, vector <int> bloom, vector <int> wilt) {
+    	n = height.size();
+        v.resize(n+1);
+	    visited.resize(n+1, false);
+        for (int i = 0; i < n; i++) v[i].first = height[i];
+        for (int i = 0; i < n; i++) v[i].second.first = bloom[i];
+        for (int i = 0; i < n; i++) v[i].second.second = wilt[i];
+        sort(v.begin(), v.end(), greater<pair<int, pair<int, int>>>());
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                arr[i][j] = (!(v[i].second.first > v[j].second.second || v[i].second.second < v[j].second.first) && v[i].first > v[j].first) ? j : i;
+        for (int i = 0; i < n; i++)
+            if (!visited[i])
+                sol(i);
+        return ans;
     }
 };
