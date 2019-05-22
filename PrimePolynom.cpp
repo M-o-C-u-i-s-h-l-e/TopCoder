77894940c4929ac940d2d13ab1e1194a6da41fac
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-05-22 19:36:05 +0530

    Create PrimePolynom.cpp

---
+++ PrimePolynom.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class PrimePolynom {
+    public:
+	int reveal(int A, int B, int C) {
+        for (int i = 0; i <= 10000; i++) {
+            long long n = A * i * i + B * i + C;
+            if (n < 0)  return i;
+            for (int j = 2; j <= sqrt(n); j++) {
+                if (n % j == 0) {
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
+};
